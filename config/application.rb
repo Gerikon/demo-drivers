3e8e9ff23c4fe2b3da593ff641287dba7127e1b5
Max Efimov <gerikon.work@gmail.com>
2017-04-13 12:34:05 +0700

    Change autoload_paths to eager_load_paths because production error.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ Bundler.require(*Rails.groups)
 
 module DemoDrivers
   class Application < Rails::Application
-    config.autoload_paths << Rails.root.join('lib')
+    # config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
